a32e7c03a03b3e89d1cd1306e0f318ff78333e11
becover <sedi.september@gmail.com>
2020-01-03 20:54:57 +0900

    class 오타수정, 구독, 디스패치 설정

--- src/index.js
+++ src/index.js
@@ -1,45 +1,68 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
-const divToggle = document.querySelector('.toggle');
-const counter = document.querySelector('h1');
-const btnIncrease = document.querySelector('#increase'),
-  btnDecrease = document.qeurySelector('#decrease');
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase"),
+  btnDecrease = document.querySelector("#decrease");
 
-const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
-const INCREASE = 'INCREASE';
-const DECREASE = 'DECREA;SE';
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
 
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
-const increase = difference => ({ type: INCREASE, difference });
-const decrease = () => ({ type: DECREASE });
+const increase = number => ({ type: INCREASE, number });
+const decrease = number => ({ type: DECREASE, number });
 
 const initialState = {
   toggle: false,
-  counter: 0,
-}
+  counter: 0
+};
 
 //state가 undefined일 때는 initialState를 기본값으로 사용
 function reducer(state = initialState, action) {
   //action.type에 따라 다른 작업을 처리함
-  switch(action.type) {
+  switch (action.type) {
     case TOGGLE_SWITCH:
       return {
         ...state, //불변성을 유지 해 주어야 한다.
-        toggle: !state.toggle,
-      }
+        toggle: !state.toggle
+      };
     case INCREASE:
       return {
         ...state,
-        counter: state.counter + action.difference,
-      }
+        counter: state.counter + action.number
+      };
     case DECREASE:
       return {
         ...state,
-        counter: state.counter -1,
-      }
+        counter: state.counter - action.number
+      };
     default:
       return state;
   }
 }
 
 const store = createStore(reducer);
+
+const render = () => {
+  const state = store.getState(); //현재 상태를 불러온다.
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+  counter.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
+
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch());
+};
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1));
+};
+btnDecrease.onclick = () => {
+  store.dispatch(decrease(1));
+};
