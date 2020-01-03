9252c8743fa5ec6106c1a49a862890e5c6e1a2cf
becover <sedi.september@gmail.com>
2020-01-03 19:28:25 +0900

    Dom reference, initializing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hello parcel!')
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector('#increase'),
+  btnDecrease = document.qeurySelector('#decrease');
+
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREA;SE';
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = difference => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+  toggle: false,
+  counter: 0,
+}
